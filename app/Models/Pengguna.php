23801fd578cb40ec4cb19747d0b30524d4ff007c
R230905 <afrizalfajri23@gmail.com>
2025-02-13 18:02:44 +0700

    feat(model): add pengguna model

---
+++ app/Models/Pengguna.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Pengguna extends Authenticatable
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
+
+    protected $table = 'pengguna';
+    protected $primaryKey = 'pengguna_id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'wilayah_id',
+        'pengurus_id',
+        'driver_id',
+        'nik',
+        'kk',
+        'nama',
+        'jenis_kelamin',
+        'tempat_lahir',
+        'tgl_lahir',
+        'email',
+        'nohp',
+        'password',
+        'alamat',
+        'rt',
+        'rw',
+        'foto_url',
+        'ttd_url',
+        'status',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        // 'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            // 'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    /**
+     * Override default authentication field (email) to no_hp.
+     */
+    public function username(): string
+    {
+        return 'nohp';
+    }
+
+}
