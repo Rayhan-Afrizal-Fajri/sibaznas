fd0a63ae957121b8ba6aa3a68a6b80c3c966fefb
R230905 <afrizalfajri23@gmail.com>
2025-02-13 18:02:24 +0700

    fix(authorize): change table for login

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -27,7 +27,7 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'no_hp' => ['required', 'string', 'exists:users,no_hp'],
+            'nohp' => ['required', 'string'],
             'password' => ['required', 'string'],
         ];
     }
@@ -41,11 +41,11 @@ class LoginRequest extends FormRequest
     {
         $this->ensureIsNotRateLimited();
 
-        if (! Auth::attempt(['no_hp' => $this->input('no_hp'), 'password' => $this->input('password')], $this->boolean('remember'))) {
+        if (! Auth::attempt(['nohp' => $this->input('nohp'), 'password' => $this->input('password')], $this->boolean('remember'))) {
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
-                'no_hp' => trans('auth.failed'),
+                'nohp' => trans('auth.failed'),
             ]);
         }
 
@@ -68,7 +68,7 @@ class LoginRequest extends FormRequest
         $seconds = RateLimiter::availableIn($this->throttleKey());
 
         throw ValidationException::withMessages([
-            'no_hp' => trans('auth.throttle', [
+            'nohp' => trans('auth.throttle', [
                 'seconds' => $seconds,
                 'minutes' => ceil($seconds / 60),
             ]),
@@ -80,6 +80,6 @@ class LoginRequest extends FormRequest
      */
     public function throttleKey(): string
     {
-        return Str::transliterate(Str::lower($this->string('no_hp')).'|'.$this->ip());
+        return Str::transliterate(Str::lower($this->string('nohp')).'|'.$this->ip());
     }
 }
