d34743d3536cdbffc6375f1da7678067346c67db
R230905 <afrizalfajri23@gmail.com>
2025-02-16 10:01:02 +0700

    chore(Seeder): add PenggunaSeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             DivisiSeeder::class,
             JabatanSeeder::class,
             PengurusSeeder::class,
+            PenggunaSeeder::class,
         ]);
     }
     
