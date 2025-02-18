e45a977e613628921b3d67e3015be027c874d77a
R230905 <afrizalfajri23@gmail.com>
2025-02-18 18:28:46 +0700

    fix(view): change layout view

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,162 +1,35 @@
-<nav x-data="{ open: false }" class="bg-white">
-    <!-- Primary Navigation Menu -->
-    <div class="flex flex-col max-w-7xl mx-auto pt-6 px-6 sm:px-20">
-        <div class="flex flex-col sm:flex-row items-center justify-between h-28">
-            <div class="flex items-center justify-center gap-2 sm:gap-6">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <img
-                            src="{{ asset('build/images/Logo-Baznas-Cilacap-tmb 1.png') }}"
-                            alt="Baznas Cilacap"
-                            class="w-16 sm:w-20">
-                    </a>
-                </div>
-                <div>
-                    <h1 class="text-md sm:text-2xl font-bold text-[#00593b]">SIBAZNAS CILACAP</h1>
-                    <p class="text-sm sm:text:xl text-[#00593b]">SISTEM INFORMASI BAZNAS CILACAP</p>
-                </div>
-            </div>
-            <div class="flex items-center gap-4 sm:gap-0">
-                <!-- Navigation Links -->
-                <div class="space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link
-                        :href="route('dashboard')"
-                        :active="request()->routeIs('dashboard')">
-                        {{ __('Home') }}
-                    </x-nav-link>
-                </div>
-
-                <div
-                    class="text-sm  text-[#00593b] space-x-8 sm:-my-px  sm:ms-10 sm:flex border-b-2 border-transparent hover:border-[#00593b]">
-                    <a href="https://wa.me/6285842716803" target="_blank" rel="noopener noreferrer">Bantuan Teknis</a>
-                </div>
+<nav class="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+    <div class="px-3 py-3 lg:px-5 lg:pl-3">
+        <div class="flex items-center justify-between">
+            <div class="flex items-center justify-start rtl:justify-end mx-6">
+                <button
+                    data-drawer-target="logo-sidebar"
+                    data-drawer-toggle="logo-sidebar"
+                    aria-controls="logo-sidebar"
+                    type="button"
+                    class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                    <span class="sr-only">Open sidebar</span>
+                    <svg
+                        class="w-6 h-6"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            clip-rule="evenodd"
+                            fill-rule="evenodd"
+                            d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+                    </svg>
+                </button>
+                <a href="{{ route('dashboard') }}" class="flex ms-2 md:me-24">
+                    <img
+                        src="{{ asset('build/images/Logo-Baznas-Cilacap-tmb 1.png') }}"
+                        class="h-8 me-3"
+                        alt="FlowBite Logo"/>
+                    <span
+                        class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap text-[#00593b]">E-DISDAY</span>
+                </a>
             </div>
         </div>
-            <hr class="border-yellow-500 my-2">
-                <div class="flex items-center ml-auto">
-                    <!-- Settings Dropdown -->
-                    <div class="sm:flex sm:items-center sm:ms-6">
-                        <x-dropdown align="right" width="48">
-                            <x-slot name="trigger">
-                                <button
-                                    class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                                    <!-- Gambar Profil atau Inisial -->
-                                    @if(Auth::user()->profile_photo)
-                                    <img
-                                        src="{{ Auth::user()->profile_photo }}"
-                                        alt="Avatar"
-                                        class="w-10 h-10 rounded-full object-cover">
-                                        @else
-                                        <div
-                                            class="w-10 h-10 rounded-full bg-gray-100 flex items-center justify-center text-gray-700 font-semibold">
-                                            {{ strtoupper(substr(Auth::user()->nama, 0, 2)) }}
-                                        </div>
-                                        @endif
-
-                                        <!-- Nama & Email -->
-                                        <div class="flex flex-col ms-3 text-left">
-                                            <span class="text-gray-700 font-semibold">{{ Auth::user()->nama }}</span>
-                                            <span class="text-gray-500 text-xs">{{ Auth::user()->pengurus->jabatan->jabatan }}</span>
-                                        </div>
-
-                                        <!-- Icon Dropdown -->
-                                        <div class="ms-2">
-                                            <svg
-                                                class="fill-current h-4 w-4"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                viewBox="0 0 20 20">
-                                                <path
-                                                    fill-rule="evenodd"
-                                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                    clip-rule="evenodd"/>
-                                            </svg>
-                                        </div>
-                                    </button>
-
-                                </x-slot>
-
-                                <x-slot name="content">
-                                    <x-dropdown-link :href="route('profile.edit')">
-                                        {{ __('Profile') }}
-                                    </x-dropdown-link>
-
-                                    <!-- Authentication -->
-                                    <form method="POST" action="{{ route('logout') }}">
-                                        @csrf
-
-                                        <x-dropdown-link
-                                            :href="route('logout')"
-                                            onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                            {{ __('Log Out') }}
-                                        </x-dropdown-link>
-                                    </form>
-                                </x-slot>
-                            </x-dropdown>
-                        </div>
-
-                        <!-- Hamburger -->
-                        {{-- <div class="-me-2 flex items-center sm:hidden">
-                            <button
-                                @click="open = ! open"
-                                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                                <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                                    <path
-                                        :class="{'hidden': open, 'inline-flex': ! open }"
-                                        class="inline-flex"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M4 6h16M4 12h16M4 18h16"/>
-                                    <path
-                                        :class="{'hidden': ! open, 'inline-flex': open }"
-                                        class="hidden"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M6 18L18 6M6 6l12 12"/>
-                                </svg>
-                            </button>
-                        </div> --}}
-                    </div>
-                </div>
-
-                <!-- Responsive Navigation Menu -->
-                {{-- <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-                    <div class="pt-2 pb-3 space-y-1">
-                        <x-responsive-nav-link
-                            :href="route('dashboard')"
-                            :active="request()->routeIs('dashboard')">
-                            {{ __('Dashboard') }}
-                        </x-responsive-nav-link>
-                    </div>
-
-                    <!-- Responsive Settings Options -->
-                    <div class="pt-4 pb-1 border-t border-gray-200">
-                        <div class="px-4">
-                            <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                            <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-                        </div>
-
-                        <div class="mt-3 space-y-1">
-                            <x-responsive-nav-link :href="route('profile.edit')">
-                                {{ __('Profile') }}
-                            </x-responsive-nav-link>
-
-                            <!-- Authentication -->
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
-
-                                <x-responsive-nav-link
-                                    :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                                    {{ __('Log Out') }}
-                                </x-responsive-nav-link>
-                            </form>
-                        </div>
-                    </div>
-                </div> --}}
-
-            </nav>
+    </div>
+</nav>
