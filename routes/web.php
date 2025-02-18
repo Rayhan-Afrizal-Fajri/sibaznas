bf582cca3253a45853cf12a12f9859935e64a93f
R230905 <afrizalfajri23@gmail.com>
2025-02-18 13:22:34 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,11 @@ Route::get('/home', function () {
     return view('home');
 })->middleware(['auth', 'verified'])->name('home');
 
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
