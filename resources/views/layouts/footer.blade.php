370301b070ecd5c9deec032a77f5e882bb6251ca
R230905 <afrizalfajri23@gmail.com>
2025-02-18 18:28:27 +0700

    feat(layout view): add footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-[#00593b] text-white text-center py-2 mt-8 border-b-4 border-yellow-500">
+    <div class="container mx-auto">
+        <a href="https://baznas-cilacap.or.id/" target="_blank" class="text-sm">&copy; {{ date('Y') }} BAZNAS CILACAP</a>
+    </div>
+</footer>
