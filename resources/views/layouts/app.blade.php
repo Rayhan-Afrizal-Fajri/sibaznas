59c8501e1d9bd2492f62db434ddad30c109364a6
R230905 <afrizalfajri23@gmail.com>
2025-02-24 13:50:58 +0700

    fix view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,6 +42,8 @@
         <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
         <script
             src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js"></script>
+
     </head>
 
     <body class="font-sans antialiased">
